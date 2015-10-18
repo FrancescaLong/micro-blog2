6e7e10c96adeaf7b60a3a973ca9aac274bd6429c
FrancescaLong <francesca_long@hotmail.com>
2015-10-18 16:11:09 -0700

    the ejs and css files is now linked

--- server.js
+++ server.js
@@ -2,19 +2,17 @@
 console.log('Sanity Check: Server JS is working!');
 
 // REQUIREMENTS //
-var express = require('express'),
-    app = express(),
-    path = require('path'),
-    bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 
 // CONFIG //
-// set ejs as view engine
-app.set('view engine', 'ejs');
-// serve js & css files
-//app.use("/static", express.static("public")); 
-app.use(express.static('public'));
-// body parser config to accept our datatypes
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');  // set ejs as view engine
+//app.use("/static", express.static("public"));  // serve js & css files
+app.use(express.static('public'));  // serve js & css files
+app.use(bodyParser.urlencoded({ extended: true }));  // body parser config to accept our datatypes
 
 
 
@@ -28,19 +26,53 @@ var posts =[
 ];
 
 
-app.get('/', function (req, res){
+app.get("/", function (req, res){
 	res.render('index', {posts: posts});
 });
 
+
+
 app.get("/api/posts", function (req, res){
-	res.json(posts);
+	res.render('views/index.ejs', {posts: posts});
+  if (posts.length !== 0){
+	  newPost.id = posts[posts.length - 1].id + 1;      // add a unique id
+  } else {
+  	newPost.id = 0;
+  }
+  posts.push(newPost);    // add new post to DB (which, in this case, is an array)
+  res.json(newPost);   // send a response with newly created object
 });
 
 
-app.post('/api/posts', function (req, res){
+
+app.post("/api/posts", function (req, res){
   console.log(req.body);
   var newPost = {};
 });
+
+
+// api route to delete a food
+app.delete("/api/posts/:id", function (req, res){
+  var targetId = parseInt(req.params.id);    // set the value of the id
+  //var targetItem = findWhere(posts {id: targetId});    // find item in the array matching the id
+  var targetItem = findWhere(posts.id === targetID);
+  var index = posts.indexOf(targetItem);    // get the index of the found item
+  posts.splice(index, 1);    // remove the item at that index, only remove 1 item
+  res.json(targetItem);    // send back deleted object
+});
+
+
+// api route to update a post
+app.patch("/api/posts/:id", function(req, res){
+  var targetId = parseInt(req.params.id);    // set the value of the id
+  var targetItem = findWhere(posts, {id: targetId});    // find item in the array matching the id
+  // edit object, but don't change name or yumminess 
+  // if the request's values for them are blank 
+  targetItem.post = req.body.post || targetItem.post;
+  res.json(targetItem);    // send back edited object
+});
+
+
 
 
 app.listen(3000, function (){
