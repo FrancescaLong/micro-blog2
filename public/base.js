3294e34e8c3d14c87581f390eb6ec0a2e99e48ac
FrancescaLong <francesca_long@hotmail.com>
2015-10-28 21:45:21 -0700

    more updates

--- public/base.js
+++ public/base.js
@@ -2,55 +2,128 @@ console.log("Sanity Check: Base JS is working!");
 
 $(document).ready(function(){
 
- var posts = {
-  	id: 1,
- 	post: ''
- };
-	//submitForm(posts);
+ var blogPosts = [{
+  	index: 1,
+ 	entry: ''
+ }];
+	//submitForm(blogPosts);
 	//deleteButton();
 	
-//$("label").innerHTML("Last 10 out of " + posts.length + " entries..");
-$("label").html("Last 10 out of " + posts.length + " entries..");
+//$("label").innerHTML("Last 10 out of " + blogPosts.length + " entries..");
+$("label").html("Last 10 out of " + blogPosts.length + " entries..");
 
-/*  My original code that did post the entries 
+$("#new-post-form").on("submit", function() {
+	event.preventDefault();
+	var value = $('#inputBox').val();
+	console.log("top form submitted " + value);
+});
+
+/*
+//  My original code that did post the entries 
 $("#submitButton").on("click", function(){
+	console.log("submitButton clicked");
 	event.preventDefault();
 	var value = $("#inputBox").val();
-	var html = posts.unshift("<li class='postsToPresent'>" + value + "<span class='glyphicon glyphicon-remove pull-right'></span></li>");
-	$("ul").append(posts[0]);
-	alert(posts);
-	//console.log(posts);
-	//console.log($(posts).length);
+	var html = ("<li class='postsToPresent'>" + value + "<span class='glyphicon glyphicon-remove pull-right'></span></li>");
+	var index = 1;
+	blogPosts.push({index: index += 1, entry: html});
+	$("ul").append(blogPosts[0]);
+	//alert(blogPosts);
+	//console.log(blogPosts);
+	//console.log($(blogPosts).length);
 	});
+
+$.ajax({
+  url: "test.html",
+  context: document.body
+}).done(function() {
+  $( this ).addClass( "done" );
+});
 */
 
-var submitForm = function(posts){
-	var index = 1;
-	$("#submitButton").on("click", function(){
+
+/* from Margaux
+function MyBlog(){
+	this.entries = [];	//our backing array of entries
+
+	// use JQuery selectors to find the important elements
+	// (text box, button, list box)
+	this.$pushMeButton = $('.btn');
+	this.$entry = $('#myText');
+	this.$blogDiv = $('.list-group');
+
+	//assign onclick handler to button and connect to our createItem() function
+	var blog = this;
+
+	this.$pushMeButton.on('click', function(event){
 		event.preventDefault();
-		var value = $("#inputBox").val();
-		var html = posts.push("<li id=" + 
-					id + 
-					"class='postsToPresent'>" + 
-					value + 
-					"<span class='glyphicon glyphicon-remove pull-right'></span></li>");
+		blog.createItem(blog.$entry.val());
+	
+		$.ajax({
+		  method: "POST",
+		  url: "/api/post",
+		  data: $(this).serialize(),
+		  success: console.log ("Woohoo!")
+		});
+	});
+
+}
+*/
+
+// from Alex - modified with Margaux's code
+var submitForm = function(blogPosts){
+	var index = 1;
+	this.$submitButton = $('.submitButton');
+	this.$entry = $('#inputBox');
+	this.$blogDiv = $('.list-group-item');
+
+	var blog = this; //don't know if I need this
+
+	this.$('#submitButton').on('click', function(event){
+		event.preventDefault();
+		var value = $('#inputBox').val();
+		console.log("Button submit is working " + value);
+		var html = blogPosts.push("<li id=" + id + "class='postsToPresent'>" + value + "<span class='glyphicon glyphicon-remove pull-right'></span></li>");
 		index += 1;
-		$("ul").append(posts[0]);
-		console.log(posts);
-		alert(posts);
+		$.ajax({
+			method: 'POST',
+			url: '/api/blogPost',
+			data: $(this).serialize(),
+			success: console.log("it's finally posting!!!!!!!")
+		});
+		//$("ul").append(blogPosts[0]);
+		console.log(blogPosts);
+		alert(blogPosts);
 		});
 };
 
 
-var addEventListenerToDeleteButton = function(posts){
+
+/* from Alex
+var submitForm = function(blogPosts){
+	var index = 1;
+	$("#submitButton").on("click", function(){
+		event.preventDefault();
+		var value = $("#inputBox").val();
+		var html = blogPosts.push("<li id=" + id + "class='postsToPresent'>" + value + "<span class='glyphicon glyphicon-remove pull-right'></span></li>");
+		index += 1;
+		$("ul").append(blogPosts[0]);
+		console.log(blogPosts);
+		alert(blogPosts);
+		});
+};
+*/
+
+var addEventListenerToDeleteButton = function(blogPosts){
 	$(".glyphicon-remove").on("click", function(){
-		deleteItem(posts);
+		deleteItem(blogPosts);
 		index -= 1;
 	});
 };
 
-var deleteItem = function(posts){
-	posts.splice($(this).id,1);
+
+var deleteItem = function(blogPosts){
+	blogPosts.splice($(this).id,1);
 	$("ul").remove($(this).parent());
 };
 
--- server.js
+++ server.js
@@ -6,71 +6,97 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+var findWhere = require('./utils/findWhere.js');
 
 // CONFIG //
-
 app.set('view engine', 'ejs');  // set ejs as view engine
 //app.use("/static", express.static("public"));  // serve js & css files
 app.use(express.static('public'));  // serve js & css files
 app.use(bodyParser.urlencoded({ extended: true }));  // body parser config to accept our datatypes
 
-
-
 //DATA
-var posts =[
-  {id: 0, post: "ummmm post 1"},
-  {id: 1, post: "little boo is asleep"},
-  {id: 2, post: "bunny is out walking Charlie"},
-  {id: 3, post: "It is Saturday night and I am trying to make this work"},
-  {id: 4, post: "hmmm, I hope this works"}
+var blogPosts =[
+  {id: 0, entry: "ummmm post 1"},
+  {id: 1, entry: "little boo is asleep"},
+  {id: 2, entry: "bunny is out walking Charlie"},
+  {id: 3, entry: "It is Saturday night and I am trying to make this work"},
+  {id: 4, entry: "hmmm, I hope this will work at some point"}
 ];
 
 
-app.get("/", function (req, res){
-	res.render('index', {posts: posts});
+// ROUTES //
+app.get("/", function (req, res){    // render index.html and send with blogPosts data filled in
+  res.render('index', {blogPosts: blogPosts});
 });
 
+// api route to get all blogPosts (we don't use)
+app.get("/api/blogPosts", function (req, res){ 
+  res.json(blogPosts);   // send post data as JSON
+});
+
+/* Marguax's code
+app.post('/api/post', function (request, response){
+   console.log(request.body);
+   var newPost = post.create(request.body, function(error, newPost){
+     console.log(newPost);
+   });
+
+   response.json(newPost);
+ });
+*/
 
 
-app.get("/api/posts", function (req, res){
-	res.render('views/index.ejs', {posts: posts});
-  if (posts.length !== 0){
-	  newPost.id = posts[posts.length - 1].id + 1;      // add a unique id
+// api route to create new post
+app.post("/api/blogPosts", function (req, res){
+  var newPost = post.create(request.body, function(error, newPost){
+    console.log(newPost);
+    }); 
+  /*if (blogPosts.length !== 0){  // add a unique id
+    newPost.id = blogPosts[blogPosts.length - 1].id + 1;
   } else {
-  	newPost.id = 0;
+    newPost.id = 0;
+  }*/
+  blogPosts.push(newPost);   // add new post to DB (which, in this case, is an array)  
+  res.json(newPost);  // send a response with newly created object
+});
+
+/* toEatly code
+app.post("/api/blogPosts", function (req, res){
+  var newPost = req.body;  
+  if (blogPosts.length !== 0){  // add a unique id
+    newPost.id = blogPosts[blogPosts.length - 1].id + 1;
+  } else {
+    newPost.id = 0;
   }
-  posts.push(newPost);    // add new post to DB (which, in this case, is an array)
-  res.json(newPost);   // send a response with newly created object
+  blogPosts.push(newPost);   // add new post to DB (which, in this case, is an array)  
+  res.json(newPost);  // send a response with newly created object
 });
+*/
 
-
-
-app.post("/api/posts", function (req, res){
-  console.log(req.body);
-  var newPost = {};
-});
-
-
-// api route to delete a food
-app.delete("/api/posts/:id", function (req, res){
+// api route to delete a post
+app.delete("/api/blogPosts/:id", function (req, res){
   var targetId = parseInt(req.params.id);    // set the value of the id
-  //var targetItem = findWhere(posts {id: targetId});    // find item in the array matching the id
-  var targetItem = findWhere(posts.id === targetID);
-  var index = posts.indexOf(targetItem);    // get the index of the found item
-  posts.splice(index, 1);    // remove the item at that index, only remove 1 item
+  var targetItem = findWhere(blogPosts, {id: targetId});    // find item in the array matching the id
+  var index = blogPosts.indexOf(targetItem);    // get the index of the found item
+  blogPosts.splice(index, 1);    // remove the item at that index, only remove 1 item
   res.json(targetItem);    // send back deleted object
 });
 
 
 // api route to update a post
-app.patch("/api/posts/:id", function(req, res){
+app.patch("/api/blogPosts/:id", function(req, res){
   var targetId = parseInt(req.params.id);    // set the value of the id
-  var targetItem = findWhere(posts, {id: targetId});    // find item in the array matching the id
+  var targetItem = findWhere(blogPosts, {id: targetId});    // find item in the array matching the id
   // edit object, but don't change name or yumminess 
   // if the request's values for them are blank 
   targetItem.post = req.body.post || targetItem.post;
   res.json(targetItem);    // send back edited object
 });
+
+
 
 
 
